2c3f6cc349453e826eec0e9e75ca3ab04a96cce9
Daniel King <daniel.king-4@student.manchester.ac.uk>
2020-10-25 11:42:19 +0000

    Fixed pet drinking mechanics

--- tests/games/stendhal/server/entity/creature/BabyDragonTest.java
+++ tests/games/stendhal/server/entity/creature/BabyDragonTest.java
@@ -13,15 +13,20 @@
 package games.stendhal.server.entity.creature;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.item.ConsumableItem;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.MockStendlRPWorld;
 import marauroa.common.game.RPObject;
@@ -34,6 +39,11 @@ public class BabyDragonTest {
 	public static void setUpBeforeClass() throws Exception {
 		BabyDragonTestHelper.generateRPClasses();
 		MockStendlRPWorld.get();
+	}
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		MockStendlRPWorld.reset();
 	}
 
 	List<String> foods = Arrays.asList("ham", "pizza", "meat");
@@ -70,6 +80,34 @@ public class BabyDragonTest {
 		template.put("hp", 30);
 		final BabyDragon drako = new BabyDragon(template, PlayerTestHelper.createPlayer("bob"));
 		assertThat(drako.getFoodNames(), is(foods));
+	}
+	
+	/**
+	 * Tests for whether pets will drink potions when their hp is above 100 and when their hp is below 100.
+	 */
+	@Test
+	public void testPetPotionDrinkingLogic() {
+		final Map<String, String> attributes = new HashMap<String, String>();
+		
+		final StendhalRPZone zone = new StendhalRPZone("zone");
+		final Player bob = PlayerTestHelper.createPlayer("bob");
+		zone.add(bob);
+		final BabyDragon testPet = new BabyDragon(bob);
+		testPet.setPosition(0, 6);
+		testPet.damage(400, bob);
+		attributes.put("amount", "100");
+		attributes.put("quantity", "1");
+		attributes.put("regen", "100");
+		attributes.put("frequency", "1");
+		ConsumableItem testPotion = new ConsumableItem("potion", "drink", "potion", 
+														attributes);
+		zone.add(testPotion);
+		testPotion.setPosition(0,6);
+		testPet.logic();
+		assertEquals(100, testPet.getHP());
+		testPet.damage(50, bob);
+		testPet.logic();
+		assertEquals(150, testPet.getHP());
 	}
 
 }
