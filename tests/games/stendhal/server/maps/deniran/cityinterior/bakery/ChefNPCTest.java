55a0669571860a67f596d05e412e1560ceb23221
Alex Iordachescu <alex.iordachescu@student.manchester.ac.uk>
2020-10-26 14:55:03 +0000

    Initialized zone BeforeClass

--- tests/games/stendhal/server/maps/deniran/cityinterior/bakery/ChefNPCTest.java
+++ tests/games/stendhal/server/maps/deniran/cityinterior/bakery/ChefNPCTest.java
@@ -3,15 +3,23 @@ package games.stendhal.server.maps.deniran.cityinterior.bakery;
 import static org.junit.Assert.*;
 import static utilities.SpeakerNPCTestHelper.getReply;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.fsm.Engine;
+import utilities.QuestHelper;
 import utilities.ZonePlayerAndNPCTestImpl;
 
-public class ChefNPCTest extends ZonePlayerAndNPCTestImpl {
+public class ChefNPCTest  extends ZonePlayerAndNPCTestImpl {
 
-	private static final String ZONE_NAME = "testzone";
+	private static final String ZONE_NAME = "test_zone";
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+		setupZone(ZONE_NAME);
+	}
 	
 	public ChefNPCTest() {
 		setNpcNames("Patrick");
@@ -24,10 +32,10 @@ public class ChefNPCTest extends ZonePlayerAndNPCTestImpl {
 	 */
 	@Test
 	public void testHiAndJob() {
-
+				
 		final SpeakerNPC npc = getNPC("Patrick");
 		final Engine en = npc.getEngine();
-
+		
 		en.step(player, "hi");
 		assertEquals(
 				"Hello and welcome to Deniran Bakery.",
