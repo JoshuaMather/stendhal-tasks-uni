961f205d4927c72a19685bb1bcc8f339a96a15f1
Oliver Pryde <oliver.pryde@student.manchester.ac.uk>
2020-11-13 16:11:50 +0000

    Create base test methods for SluggishWandTest

---
+++ tests/games/stendhal/server/entity/item/SluggishWandTest.java
@@ -0,0 +1,51 @@
+package games.stendhal.server.entity.item;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SluggishWandTest {
+
+	@Test
+	public void shouldApplyHeavyStatusAttackerToCasterOnEquip() {
+		// Set up the fixture
+		
+		// Make a zone
+		
+		// Summon the item into bag
+		
+		// assert status effect isnt in list
+		
+		// Prepare the inputs
+		
+		// Execute the test
+		// equip item
+		
+		// Check the result
+		// assert status effect is in list
+		fail("Not yet implemented");
+		
+		// Tidy up
+		
+	}
+	
+	@Test
+	public void shouldRemoveHeavyStatusAttackerFromCasterOnUnequip() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void shouldApplyHeavyStatusToCreatureOnAttack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void shouldApplyHeavyStatusToPlayerOnAttack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void shouldDealNoDamageOnAttack() {
+		fail("Not yet implemented");
+	}
+}
