eeca9d66e6712a2375266c3fbb8c206cbadaae6f
Oliver Pryde <oliver.pryde@student.manchester.ac.uk>
2020-11-13 17:31:19 +0000

    Create tests for SluggishWand
    Added the three test methods: shouldApplyHeavyStatusToCreatureOnAttack,
    to check that when a player attacks a target with the wand equipped a
    HEAVY status will be applied to the target;
    shouldReduceBaseSpeedOfCreatureOnAttack, to check that the targeted
    creature based target's base speed has been halved when attacked by a
    player with the wand equipped; shouldReduceBaseSpeedOfPlayerOnAttack, to
    check that the targeted player's base speed has been halved when
    attacked by a player with the wand equipped. Also added SluggishWand
    stub class to allow the creation of the tests.

---
+++ src/games/stendhal/server/entity/item/SluggishWand.java
@@ -0,0 +1,12 @@
+package games.stendhal.server.entity.item;
+
+import java.util.Map;
+
+public class SluggishWand extends Item{
+
+	public SluggishWand(String name, String clazz, String subclass, Map<String, String> attributes) {
+		super(name, clazz, subclass, attributes);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- tests/games/stendhal/server/entity/item/SluggishWandTest.java
+++ tests/games/stendhal/server/entity/item/SluggishWandTest.java
@@ -1,51 +1,125 @@
 package games.stendhal.server.entity.item;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class SluggishWandTest {
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.rp.StendhalRPAction;
+import games.stendhal.server.entity.creature.Creature;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.status.StatusType;
+import games.stendhal.server.maps.MockStendlRPWorld;
+import marauroa.server.game.db.DatabaseFactory;
+import utilities.PlayerTestHelper;
 
-	@Test
-	public void shouldApplyHeavyStatusAttackerToCasterOnEquip() {
-		// Set up the fixture
-		
-		// Make a zone
-		
-		// Summon the item into bag
-		
-		// assert status effect isnt in list
-		
-		// Prepare the inputs
-		
-		// Execute the test
-		// equip item
-		
-		// Check the result
-		// assert status effect is in list
-		fail("Not yet implemented");
-		
-		// Tidy up
-		
+public class SluggishWandTest {
+	private StendhalRPZone zone;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		MockStendlRPWorld.get();
+		new DatabaseFactory().initializeDatabase();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		PlayerTestHelper.removeAllPlayers();
 	}
 	
-	@Test
-	public void shouldRemoveHeavyStatusAttackerFromCasterOnUnequip() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() throws Exception {
+		zone = new StendhalRPZone("zone", 20, 20);
+		zone.protectionMap.init(1, 1);
+		MockStendlRPWorld.get().addRPZone(zone);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		MockStendlRPWorld.get().removeZone(zone);
+
 	}
 	
 	@Test
 	public void shouldApplyHeavyStatusToCreatureOnAttack() {
-		fail("Not yet implemented");
+		
+		// Set up the fixture
+		final Player player = PlayerTestHelper.createPlayer("hyde");
+		final Item wand = SingletonRepository.getEntityManager().getItem("sluggish_wand");
+		Creature victim = SingletonRepository.getEntityManager().getCreature("mouse");
+		
+		// Equip sluggish wand to player
+		player.equip("lhand", wand);
+		
+		// Add target to zone
+		zone.add(victim);
+		
+		// Check target doesn't already have HEAVY status
+		assertFalse(victim.hasStatus(StatusType.HEAVY));
+
+		// Get player to attack target
+		StendhalRPAction.startAttack(player, victim);
+		player.stopAttack();
+		
+		// Check target now has HEAVY status after attack
+		assertTrue(victim.hasStatus(StatusType.HEAVY));
 	}
 	
 	@Test
-	public void shouldApplyHeavyStatusToPlayerOnAttack() {
-		fail("Not yet implemented");
+	public void shouldReduceBaseSpeedOfCreatureOnAttack() {
+		
+		// Set up the fixture
+		final Player player = PlayerTestHelper.createPlayer("hyde");
+		final Item wand = SingletonRepository.getEntityManager().getItem("sluggish_wand");
+		Creature victim = SingletonRepository.getEntityManager().getCreature("mouse");
+		
+		// Equip sluggish wand to player
+		player.equip("lhand", wand);
+		
+		// Add target to zone
+		zone.add(victim);
+		
+		// Get initial values
+		double initBaseSpeed = victim.getBaseSpeed();
+
+		// Get player to attack target
+		StendhalRPAction.startAttack(player, victim);
+		player.stopAttack();
+		
+		// Check target is now slower than it was before attack
+		assertEquals(initBaseSpeed/2, victim.getBaseSpeed(), 0.001);
 	}
 	
 	@Test
-	public void shouldDealNoDamageOnAttack() {
-		fail("Not yet implemented");
+	public void shouldReduceBaseSpeedOfPlayerOnAttack() {
+		// Set up the fixture
+		final Player player = PlayerTestHelper.createPlayer("hyde");
+		final Player victim = PlayerTestHelper.createPlayer("bob");
+		final Item wand = SingletonRepository.getEntityManager().getItem("sluggish_wand");
+		
+		// Equip sluggish wand to player
+		player.equip("lhand", wand);
+		
+		// Add target and player to zone
+		zone.add(victim);
+		zone.add(player);
+		
+		// Get initial values
+		double initBaseSpeed = victim.getBaseSpeed();
+
+		// Get player to attack target
+		StendhalRPAction.startAttack(player, victim);
+		player.stopAttack();
+		
+		// Check target is now slower than it was before attack
+		assertEquals(initBaseSpeed/2, victim.getBaseSpeed(), 0.001);
 	}
+
 }
